2aeb1059995f32cddc0f0014525923e2985b5e2a
Toru, Kobayashi <koba0004@gmail.com>
2014-07-27 00:49:31 +0900

    Separte component by v-view

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ var Vue       = require('vue'),
     director  = require('director')
 ;
 
+Vue.config("debug", true);
+
+// component
+Vue.component('top', require('./top/'));
+Vue.component('login', require('./login/'));
+
 // create App
 require('insert-css')(require('./index.styl'));
 var app = new Vue({
@@ -11,18 +17,25 @@ var app = new Vue({
   className: "container",
   template: require('./index.html'),
   data: {
+    view: '',
     name: "vue",
   }
 });
 
 Parse.initialize("QvIeb4Xn9a3HrGbgTVMIMfrtltKwGBvM4ycpMEBk", "2V0zGyKEJxyinKfrPMnxlcKGxJlQ7exCwBd6yTgS");
 
-Parse.User.logIn("my name", "my pass", {
-  success: function(user) {
-    app.name = user.get("username");
-  },
-  error: function(user, error) {
-    alert("Error: " + error.code + " " + error.message);
-  }
-});
+if (Parse.User.current()) {
+  app.view = "top";
+  Parse.User.logIn("my name", "my pass", {
+    success: function(user) {
+      app.name = user.get("username");
+    },
+    error: function(user, error) {
+      alert("Error: " + error.code + " " + error.message);
+    }
+  });
+} else {
+  app.view = "login";
+}
+
 
---
+++ src/top/index.js
@@ -0,0 +1,8 @@
+require('insert-css')(require('./index.styl'));
+
+var Vue = require('vue');
+
+module.exports = Vue.extend({
+  template: require('./index.html')
+});
+
