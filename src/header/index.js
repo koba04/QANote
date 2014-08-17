bdc1b2d4632557159902bef4b5088ecd183b2ac3
Toru, Kobayashi <koba0004@gmail.com>
2014-08-18 00:25:32 +0900

    Improved header title

--- src/header/index.js
+++ src/header/index.js
@@ -8,9 +8,22 @@ module.exports = Vue.extend({
     description: "",
   },
   created: function() {
-    if (this.$parent.view === "top") {
-      this.description = "Categories";
-    }
+    this.updateDescription(this.$parent.view);
+    this.$parent.$watch('view', function(value) {
+      this.updateDescription(value);
+    }.bind(this));
   },
+  methods: {
+    updateDescription: function(view) {
+      switch (view) {
+        case "top":
+          this.description = "Category";
+          break;
+        case "qa":
+          this.description = this.$parent.category;
+          break;
+      }
+    }
+  }
 });
 
